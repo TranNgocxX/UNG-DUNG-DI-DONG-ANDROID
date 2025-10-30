b819956d1d4da818a63336ab193e8d490875a27a
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2025-10-30 22:15:55 +0700

    Delete lib directory

--- lib/features/bookings/domain/repositories/booking_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../entities/booking_entity.dart';
-
-abstract class BookingRepository {
-  Future<void> createBooking(Booking booking);
-  Stream<List<Booking>> getBookingsByUserStream(String userId);
-}
