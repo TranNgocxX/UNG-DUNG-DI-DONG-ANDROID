988bd87c7fdae122587bf81243171c0ebb06839c
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2025-10-31 21:22:59 +0700

    Update booking_model.dart

--- lib/features/bookings/data/models/booking_model.dart
+++ lib/features/bookings/data/models/booking_model.dart
@@ -15,7 +15,6 @@ class BookingModel extends Booking {
     required super.createdAt,
   });
 
-  /// Chuyển dữ liệu Firestore -> Model
   factory BookingModel.fromMap(Map<String, dynamic> map, String id) {
     return BookingModel(
       id: id,  // id từ Firestore
@@ -33,7 +32,6 @@ class BookingModel extends Booking {
     );
   }
 
-  /// Chuyển Model -> Map để lưu Firestore (không include id, vì Firestore tự generate)
   Map<String, dynamic> toMap() {
     return {
       'userId': userId,
@@ -48,53 +46,3 @@ class BookingModel extends Booking {
     };
   }
 }
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import '../../domain/entities/booking_entity.dart';
-
-// class BookingModel extends Booking {
-//   BookingModel({
-//     required super.id,
-//     required super.userId,
-//     required super.roomId,
-//     required super.roomType,
-//     required super.roomNumber,
-//     required super.price,
-//     required super.name,
-//     required super.phone,
-//     required super.payment,
-//     required super.createdAt,
-//   });
-
-//   factory BookingModel.fromMap(Map<String, dynamic> map, String id) {
-//     return BookingModel(
-//       id: id,
-//       userId: map['userId'] ?? '',
-//       roomId: map['roomId'] ?? '',
-//       roomType: map['roomType'] ?? '',
-//       roomNumber: map['roomNumber'] ?? '',
-//       price: (map['price'] ?? 0).toDouble(),
-//       name: map['name'] ?? '',
-//       phone: map['phone'] ?? '',
-//       payment: map['payment'] ?? '',
-//       createdAt: (map['createdAt'] is Timestamp)
-//           ? (map['createdAt'] as Timestamp).toDate()
-//           : DateTime.now(),
-//     );
-//   }
-
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'userId': userId,
-//       'roomId': roomId,
-//       'roomType': roomType,
-//       'roomNumber': roomNumber,
-//       'price': price,
-//       'name': name,
-//       'phone': phone,
-//       'payment': payment,
-//       'createdAt': FieldValue.serverTimestamp(), // Đảm bảo đúng kiểu
-//     };
-//   }
-// }
-
