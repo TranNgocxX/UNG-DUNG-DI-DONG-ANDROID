f9d68c02772f33a332de40d7c03e91bbd46b770f
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2025-10-31 21:23:18 +0700

    Update booking_repository_impl.dart

--- lib/features/bookings/data/repositories/booking_repository_impl.dart
+++ lib/features/bookings/data/repositories/booking_repository_impl.dart
@@ -10,7 +10,6 @@ class BookingRepositoryImpl implements BookingRepository {
 
   @override
   Future<void> createBooking(Booking booking) async {
-    // ✅ Tạo BookingModel thủ công từ Booking (không cần factory)
     final bookingModel = BookingModel(
       id: booking.id,
       userId: booking.userId,
@@ -32,27 +31,3 @@ class BookingRepositoryImpl implements BookingRepository {
     return remoteDataSource.getBookingsByUserStream(userId);
   }
 }
-
-
-
-
-// import '../../domain/entities/booking_entity.dart';
-// import '../../domain/repositories/booking_repository.dart';
-// import '../../data/datasources/booking_remote_datasource.dart';
-// import '../../data/models/booking_model.dart';
-
-// class BookingRepositoryImpl implements BookingRepository {
-//   final BookingRemoteDataSource remoteDataSource;
-
-//   BookingRepositoryImpl(this.remoteDataSource);
-
-//   @override
-//   Future<void> createBooking(Booking booking) async {
-//     await remoteDataSource.createBooking(booking as BookingModel);
-//   }
-
-//   Stream<List<Booking>> getBookingsByUserStream(String userId) {
-//     return remoteDataSource.getBookingsByUserStream(userId);
-//   }
-
-// }
